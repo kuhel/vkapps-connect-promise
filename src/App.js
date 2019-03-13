923af40ba4e6b4c965e15612e5cedb56e4827b4b
kuhel <vorontsov.gleb@gmail.com>
2019-03-13 12:08:36 +0300

    typo

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export default class App extends Component {
             <UI.View activePanel="main">
                 <UI.Panel id="main">
                     <UI.PanelHeader>
-                        VK Connect Promise TEst v0.1.0
+                        VK Connect Promise Test v0.1.0
                     </UI.PanelHeader>
                     <UI.Group title="Data">
                         <UI.FormLayout>
